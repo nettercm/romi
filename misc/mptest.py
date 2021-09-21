8566b33003cc3f041be9599ffd8d87847319f4a1
Christian Netter <chris@chrisnetter.com>
2021-09-21 15:55:54 -0400

    mptest.py:  added separate Q for the echo

--- misc/mptest.py
+++ misc/mptest.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+ 
 import multiprocessing as mp
 import time
 import threading, queue
@@ -51,56 +53,68 @@ def bar(q,id):
                 done=True
 
 
-def ping(q,id):
+def ping(q1,q2,id):
     #signal.signal(signal.SIGINT, signal_handler)
-    #print("ping(%d) has started" % (id) )
+    print("ping(%d) has started" % (id) )
     
     #first measure latency
     t1=time.monotonic()
-    for i in range(1,1000):
-        q.put('ping')
-        e = q.get()
+    for i in range(1,100000):
+        q1.put('ping')
+        e = q2.get()
+        #print('ping',i,e)
     t2=time.monotonic()
+    print("ping: done with overhead test")
     print(i, ((t2-t1)/i)*1000000)
+    time.sleep(0.5)
 
-    #now measure cpu usage for doing put() / get() at a fixed rate of 1000 per second
+    #now measure cpu usage for doing put() / get() at a fixed rate of 100 per second
     t1=time.monotonic()
     i=0
     while time.monotonic() - t1 < 10.0:
         t = time.monotonic()
-        q.put('ping')
-        e = q.get()
+        q1.put('ping')
+        e = q2.get()
         i = i+1
-        sleep_time = 0.002 - (time.monotonic() - t)  #500Hz
+        #print('ping',i,e)
+        sleep_time = 0.005 - (time.monotonic() - t)  #200 iter per sec 
         sleep_time = sleep_time - 0.00010
         if sleep_time > 0: time.sleep(sleep_time)
-    q.put('done')
+    print("ping: done with CPU usage test")
+    q1.put('done')
     t2=time.monotonic()
     print(i, ((t2-t1)/i)*1000000)
+    time.sleep(0.5)
 
 
-def echo(q,id):
+def echo(q1,q2,id):
     #signal.signal(signal.SIGINT, signal_handler)
-    #print("echo(%d) has started" % (id) )
+    print("echo(%d) has started" % (id) )
     t1=time.monotonic()
     done = False
+    i = 0
     while not done:
-        p = q.get()
-        q.put('echo')
+        p = q1.get()
+        i = i + 1
+        #print('echo',i,p)
+        q2.put('echo')
+        #if p == 'ping':
+        #    q.put('echo')
         if p == 'done':
             done = True
-            print("echo(): received the 'done' signal");
+            print("echo(): received the 'done' signal")
  
  
 def multiprocessing_test():
     print("multiprocessing test")
     mp.set_start_method('spawn')
-    q = mp.Queue()
+    q1 = mp.Queue()
+    q2 = mp.Queue()
     print("starting p1")
-    p1 = mp.Process(target=ping, args=(q,1))
+    p1 = mp.Process(target=ping, args=(q1,q2,1))
     p1.start()
     print("starting p2")
-    p2 = mp.Process(target=echo, args=(q,2))
+    p2 = mp.Process(target=echo, args=(q1,q2,2))
     p2.start()
     #print(q.get())
     print("waiting for processes to exit...")
@@ -112,12 +126,13 @@ def multiprocessing_test():
 
 def threading_test():
     print("threading test")
-    q=queue.Queue()
+    q1=queue.Queue()
+    q2=queue.Queue()
     print("starting t1")
-    t1 = threading.Thread(target=ping, args=(q,1))
+    t1 = threading.Thread(target=ping, args=(q1,q2,1))
     t1.start()
     print("starting t2")
-    t2 = threading.Thread(target=echo, args=(q,2))
+    t2 = threading.Thread(target=echo, args=(q1,q2,2))
     t2.start()
     print("waiting for threads to exit...")
     t1.join()
