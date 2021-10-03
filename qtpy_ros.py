591028bdcec52a576a43cbdf4a00a2150612b3f7
Christian Netter <chris@chrisnetter.com>
2021-10-03 00:57:07 -0400

    qtpy_ros.py:  publish imu data - uses qtpy.py as the "driver"

---
+++ qtpy_ros.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python2
+
+# standard imports
+import time
+import signal
+import sys
+import threading
+import termios
+import fcntl
+import os
+from math import cos, sin, pi
+
+# ros imports
+import rospy
+import tf
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Point,  Pose,  Quaternion,  Twist,  Vector3,  PointStamped
+from std_msgs.msg import Int16, Int32
+
+# qtpy "driver"
+import qtpy
+
+
+
+done = False
+
+
+def signal_handler(sig, frame):
+    global done
+    print('You pressed Ctrl+C!')
+    sys.exit(0)
+    done = True
+    qtpy.done = True
+
+signal.signal(signal.SIGINT, signal_handler)
+
+rospy.init_node('imu_publisher')
+
+imu_pub = rospy.Publisher("imu", PointStamped, queue_size=5, tcp_nodelay=True)
+
+current_time = last_time = rospy.Time.now()
+
+r = rospy.Rate(100)
+
+qtpy.initialize()
+
+while not rospy.is_shutdown():
+
+    current_time = rospy.Time.now()
+
+    result = qtpy.update()
+
+    if result != True:
+        continue
+
+    t = time.monotonic()
+
+    #print("%8d, %7.3f, %8.4f, %8.4f, %11.2f, dps=%6.2f" % (qtpy.timestamp, t,
+    #    qtpy.heading_calib, qtpy.heading_uncal_new, qtpy.heading_delta_calib_accumulated, qtpy.dps))
+
+    imu_data = PointStamped()
+    imu_data.header.stamp = current_time
+    imu_data.header.frame_id = "odom"
+    imu_data.point.x = qtpy.heading_calib
+    imu_data.point.y = qtpy.heading_delta_calib_accumulated
+    imu_data.point.z = qtpy.dps
+    imu_pub.publish(imu_data)
