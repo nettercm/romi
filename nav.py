0a8a4453de28cd6931cc43886ea1db03722df86d
Christian Netter <chris@chrisnetter.com>
2022-01-24 16:38:25 -0500

    waypoint navigation: initial commit

---
+++ nav.py
@@ -0,0 +1,139 @@
+#!/usr/bin/env python3
+
+from math import sqrt, atan2, pi
+#from turtle import radians
+
+ERR_CIRCLE = 0.013  #2.5cm == 1 inch
+DEADZONE = 0.052  #3 degrees  # not used anymore
+DOWNRAMP = 0.25
+nav_angular_speed = 2.9
+nav_linear_speed = 0.3
+
+
+last_distance = 0.0
+debug = True
+
+
+
+def rads(degrees):
+    """
+    convert from degrees to radians
+    """
+    return degrees * pi / 180.0
+
+
+
+
+def sign(x):
+    """
+    return -1 if the input was negative, +1 otherwise.
+    There probably is a library function for this, but I couldn't find it...
+    """
+    if x < 0:
+        return -1
+    else:
+        return 1
+
+        
+
+
+def target_acquired(distance, last_distance):
+    """
+    returns True if distance falls within the acceptable error
+    """
+    # if we are inside the error circle, but error is improving, keep going
+    # but if we are inside the error circle and we are getting further away, we have reached
+    if distance < ERR_CIRCLE and (distance+0.001) > last_distance:
+        return True
+    else:
+        return False
+
+
+
+
+def target_vector(X_position, Y_position, Theta,   X_target, Y_target):
+    """
+    calculate distance bearing from target
+    returns the tuple (distance,bearing)
+    """
+    x = X_target - X_position
+    y = Y_target - Y_position
+    distance = sqrt( (x*x) + (y*y) )
+    bearing =  atan2 ( y, x ) - Theta
+
+    #normalize - keep bearing between -pi and +pi
+    if bearing > pi:
+        bearing = bearing - 2.0*pi
+    if bearing < -pi:
+        bearing = bearing + 2.0*pi
+
+    return (distance,bearing)
+
+
+
+
+def navigate_target(X_position, Y_position, Theta, X_target,Y_target):
+    """
+    This is the primary function provided by this module
+    produce a navigation command (desired linear + angular velocity) that will take us closer to the target
+    also indicate if we reached the target
+    """
+    global last_distance
+
+    (distance,bearing) = target_vector(X_position, Y_position, Theta, X_target,Y_target)
+    acquired = target_acquired(distance, last_distance)
+    last_distance = distance
+
+    if acquired:
+        return (True, 0.0, 0.0, distance, bearing) # we have reached - return 0.0 for commanded linear and angular velocity
+    
+    #if ( ( bearing < DEADZONE ) and ( bearing > -DEADZONE ) ) :
+    #    commanded_angular_speed = 0.0
+    #else:
+        #if bearing > DEADZONE:
+        #    commanded_angular_speed = nav_angular_speed
+        #if bearing < -DEADZONE:
+        #    commanded_angular_speed = -nav_angular_speed
+
+    # if bearing is off by >80 degrees, steer hard, otherwise, steer proportionally based on how far off we are
+    if abs(bearing) > rads(80):
+        commanded_angular_speed = sign(bearing) * nav_angular_speed
+        linear_speed_factor = 0.1
+    else:
+        commanded_angular_speed = (bearing / rads(80)) * nav_angular_speed
+        linear_speed_factor = 1.0
+
+    # as we get closer to the target, slow down more and more
+    # example: half way between DOWNRAMP and actual target, commanded linear speed will be only 50% of full speed  ( e.g. 0.1 / 0.2 = 0.5 )
+    if distance < DOWNRAMP:
+        commanded_linear_speed = nav_linear_speed * (distance / DOWNRAMP)
+    else:
+        commanded_linear_speed = nav_linear_speed
+
+    # apply the speed adjustment factor 
+    commanded_linear_speed = commanded_linear_speed * linear_speed_factor
+
+
+    return (False,commanded_linear_speed, commanded_angular_speed, distance, bearing)
+
+
+
+
+if __name__ == '__main__':
+
+    # test cases
+
+    print( target_vector(0,0,0, 0,0))
+    print( target_vector(0,0,0, 1,0))
+    print( target_vector(0,0,0, 0,1))
+    print( target_vector(0,0,0, 0,-1))
+
+    print( navigate_target(0,0,0, 1,0)  )
+ 
+    print( navigate_target(0,0,0, -1,0)  )
+    print( navigate_target(0,0,0, -1,0.1)  )
+    print( navigate_target(0,0,0, -1,-0.1)  )
+
+    print( navigate_target(0,0,0, 0,1)  )
+    print( navigate_target(0,0,0, 1,1)  )
+    print( navigate_target(0,0,0, 1,-1)  )
---
+++ nav_ros.py
@@ -0,0 +1,152 @@
+#!/usr/bin/env python3
+
+import time
+
+from jinja2 import TemplateRuntimeError
+print("%11.3f" % (time.monotonic()))  # print some time stamps for profiling purposes
+
+from geometry_msgs.msg import Point, Pose, Quaternion, Twist, Vector3
+from math import cos, sin, pi, atan2
+from std_msgs.msg import Int16, Int32
+from geometry_msgs.msg import PointStamped
+from nav_msgs.msg import Odometry
+import tf
+import rospy
+import os
+import fcntl
+import termios
+import math
+import threading
+import sys
+import signal
+import numpy as np
+
+import nav
+from scanf import scanf
+
+print("%11.3f" % (time.monotonic()))  # print some time stamps for profiling purposes
+
+'''
+ros:  
+theta = 0, going forward =>  x increases
+turning right => theta decreases
+turning left  => theta increases
+
+'''
+# these globals will get updated every time new X/Y/Theta odometry data comes in via the subscribed /odom ROS topic
+X_position = 0.0
+Y_position = 0.0
+Theta = 0.0
+odometry_ready = False
+
+done = False
+
+def signal_handler(sig, frame):
+    global done
+    print('You pressed Ctrl+C!')
+    done = True
+
+
+
+print("installing SIGINT handler")
+signal.signal(signal.SIGINT, signal_handler)
+
+
+
+
+def yaw_from_quaternion(orientation : Quaternion):
+    w = orientation.w
+    x = orientation.x
+    y = orientation.y
+    z = orientation.z
+    theta = atan2((2.0 * (w*z + x*y)), (1.0 - 2.0 * (y*y + z*z)))
+    return theta
+
+
+
+def odom_callback(msg: Odometry):
+    global odometry_ready, X_position, Y_position, Theta
+    X_position = msg.pose.pose.position.x
+    Y_position = msg.pose.pose.position.y
+    Theta = yaw_from_quaternion(msg.pose.pose.orientation)
+    odometry_ready = True
+    return
+
+
+
+# process command line parameters
+#
+if len(sys.argv) == 1:
+    args = "0 0"
+else:
+    args = sys.argv[1]+" "+sys.argv[2]
+scanf_result = scanf('%f %f',args)
+print(scanf_result)
+X_target = scanf_result[0]
+Y_target = scanf_result[1]
+
+
+
+# initialize all that ROS stuff....
+#
+rospy.init_node('nav',disable_signals=True)
+odom_sub = rospy.Subscriber("odom", Odometry,        odom_callback, tcp_nodelay=True)
+cmd_vel_pub = rospy.Publisher("cmd_vel", Twist, queue_size=5, tcp_nodelay=True)
+cmd_vel_data = Twist()
+r = rospy.Rate(20)  # loop rate of 20Hz seems sufficient
+
+
+#wait for odometry input to start arriving....otherwise we don't know our current position
+#
+t = time.monotonic()
+while (not odometry_ready) and (not done):
+    rospy.sleep(0.02)
+    if time.monotonic() > t + 1.0:
+        break
+print("%11.3f    Odometry is ready...."% (time.monotonic()))
+
+
+
+while (not rospy.is_shutdown()) and (not done):
+
+    current_time = rospy.Time.now() # ROS time...
+    t = time.monotonic()            # wall clock time.... 
+
+    # produce the navigation command and determine if we already reached the target
+    #
+    (acquired, commanded_linear_speed, commanded_angular_speed, distance, bearing) = nav.navigate_target(  X_position, Y_position, Theta,     X_target, Y_target  )
+
+    # publish the commanded speed via a standard ROS /cmd_vel mesage of type Twist
+    #
+    cmd_vel_data.angular.x = 0.0
+    cmd_vel_data.angular.y = 0.0
+    cmd_vel_data.angular.z = commanded_angular_speed 
+    cmd_vel_data.linear.x =  commanded_linear_speed
+    cmd_vel_data.linear.y =  0.0
+    cmd_vel_data.linear.z =  0.0
+    cmd_vel_pub.publish(cmd_vel_data)
+
+    print("%11.3f,   %5.2f,%5.2f,%5.2f,    %5.3f,   %5.2f,%5.2f,%5.2f" % (  t, X_position, Y_position, Theta,     distance,    bearing, commanded_linear_speed, commanded_angular_speed  ))
+
+    # reached?  then we are done
+    #
+    if acquired:
+        break
+
+    # basically a sleep(), but done in such a way that we maintain the desired loop rate,
+    # regardless of how long the body of the loop took to execute
+    r.sleep()
+
+
+cmd_vel_data.angular.x = 0.0
+cmd_vel_data.angular.y = 0.0
+cmd_vel_data.angular.z = 0.0
+cmd_vel_data.linear.x =  0.0
+cmd_vel_data.linear.y =  0.0
+cmd_vel_data.linear.z =  0.0
+cmd_vel_pub.publish(cmd_vel_data)
+print("Exiting...")
+rospy.sleep(0.2)
+rospy.signal_shutdown("Ctrl-C")
+rospy.sleep(0.2)
+
